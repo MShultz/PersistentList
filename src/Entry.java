3d952f269b3db73baab4e1bddfff7a86534768b1
MShultz <mshultz@student.neumont.edu>
2016-07-14 14:15:39 -0600

    Initial Commit. Invalid return values. Method stubs created

---
+++ src/Entry.java
@@ -0,0 +1,30 @@
+
+public class Entry {
+	 private String string;
+     private long value;
+     private long link;
+
+     public Entry(String string, long value, long link){
+    	 
+     }
+
+     public String getString(){
+    	 return "Invalid String";
+     }
+
+     public long getValue(){
+    	 return -1;
+     }
+
+     public void setValue(long value){
+    	 
+     }
+
+     public long getLink(){
+    	 return -1;
+     }
+
+     public void setLink(long link){
+    	 
+     }
+}
---
+++ src/ListFile.java
@@ -0,0 +1,30 @@
+
+public class ListFile {
+	public static void initialize(String listFileName){
+		
+	}
+
+	public static void delete(String listFileName){
+		
+	}
+
+	public ListFile(String listFileName){
+		
+	}
+
+	public void close(){
+		
+	}
+
+	public long newEntry(Entry entry){
+		return -1;
+	}
+
+	public Entry get(long offset){
+		return new Entry("Invalid", -1, -1);
+	}
+
+	public void putEntry(long offset, Entry entry){
+		
+	}
+}
